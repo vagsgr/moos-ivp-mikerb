4399f865b9cef9061f33545c7726116634a59595
vagsgr <vagsiatis@gmail.com>
2026-06-19 03:37:57 -0400

    Lab 09: pGenRescue dynamic rescue path planning (Asg 2 & 4)

--- src/pGenRescue/GenRescue.cpp
+++ src/pGenRescue/GenRescue.cpp
@@ -3,9 +3,12 @@
 /*    ORGN: MIT                                             */
 /*    FILE: GenRescue.cpp                                   */
 /*    DATE: April 18th, 2022                                */
+/*    MOD : Lab 09 Assignment 4 - dynamic rescue planning   */
+/*          (Evangelos Siatis)                              */
 /************************************************************/
 
 #include <iterator>
+#include <cstdlib>
 #include "GenRescue.h"
 #include "MBUtils.h"
 #include "ColorParse.h"
@@ -26,8 +29,18 @@ GenRescue::GenRescue()
   // Initialize state variables
   m_nav_x = 0;
   m_nav_y = 0;
-  m_nav_x_set = 0;
-  m_nav_y_set = 0;
+  m_nav_x_set = false;
+  m_nav_y_set = false;
+
+  // [Lab09] config defaults
+  m_update_var = "SURVEY_UPDATE";
+
+  // [Lab09] state/metrites
+  m_plan_pending       = false;
+  m_total_alerts       = 0;
+  m_total_new_swimmers = 0;
+  m_total_found_msgs   = 0;
+  m_plans_posted       = 0;
 }
 
 //---------------------------------------------------------
@@ -44,9 +57,9 @@ bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
     string sval = msg.GetString();
 
     bool handled = true;
-    if(key == "SWIMMER_ALERT") 
+    if(key == "SWIMMER_ALERT")
       handled = handleMailNewSwimmer(sval);
-    else if(key == "FOUND_SWIMMER") 
+    else if(key == "FOUND_SWIMMER")
       handled = handleMailFoundSwimmer(sval);
     else if(key == "NAV_X") {
       m_nav_x = msg.GetDouble();
@@ -59,14 +72,14 @@ bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
 
     else if(key != "APPCAST_REQ") // handle by AppCastingMOOSApp
       handled = false;
-    
+
     if(!handled)
       reportRunWarning("Unhandled Mail: " + key +"=" + sval);
-    
+
   }
   return(true);
 }
- 
+
 //---------------------------------------------------------
 // Procedure: OnConnectToServer()
 
@@ -82,10 +95,13 @@ bool GenRescue::OnConnectToServer()
 bool GenRescue::Iterate()
 {
   AppCastingMOOSApp::Iterate();
-  
-  //if(m_plan_pending)
-  if((m_iteration % 20) == 0)
+
+  // [Lab09] Ksana-sxediazoume MONO otan exoume nea plhroforia (m_plan_pending)
+  // KAI exoume egkyro NAV fix (alliws to greedy 8a ksekinouse apo to (0,0)).
+  if(m_plan_pending && m_nav_x_set && m_nav_y_set) {
     postShortestPath();
+    m_plan_pending = false;
+  }
 
   AppCastingMOOSApp::PostReport();
   return(true);
@@ -96,11 +112,11 @@ bool GenRescue::Iterate()
 
 bool GenRescue::OnStartUp()
 {
-  AppCastingMOOSApp::OnStartUp(); 
+  AppCastingMOOSApp::OnStartUp();
 
   STRING_LIST sParams;
   m_MissionReader.GetConfiguration(GetAppName(), sParams);
-  
+
   STRING_LIST::iterator p;
   for(p=sParams.begin(); p!=sParams.end(); p++) {
     string sLine  = *p;
@@ -108,9 +124,11 @@ bool GenRescue::OnStartUp()
     string value  = sLine;
     if(param == "vname")
       m_vname = value;
+    else if((param == "update_var") && (value != ""))
+      m_update_var = value;       // [Lab09] override (default SURVEY_UPDATE)
   }
-  
-  RegisterVariables();	
+
+  RegisterVariables();
   return(true);
 }
 
@@ -122,97 +140,144 @@ void GenRescue::RegisterVariables()
   AppCastingMOOSApp::RegisterVariables();
   Register("SWIMMER_ALERT", 0);
   Register("FOUND_SWIMMER", 0);
+  Register("NAV_X", 0);          // [Lab09] xreiazetai gia to greedy start point
+  Register("NAV_Y", 0);
 }
 
 
 //---------------------------------------------------------
 // Procedure: handleMailNewSwimmer()
+//   String: "x=23, y=54, id=04"  (apo to uFldRescueMgr, ka8e 15s)
+//   [Lab09] kratame ka8e MONADIKO swimmer (dedupe by id) kai zhtame replan
+//           mono otan o swimmer einai ontws kainourios.
 
 bool GenRescue::handleMailNewSwimmer(string str)
 {
+  m_total_alerts++;
+
+  string id;
+  double x = 0, y = 0;
+  bool x_set = false, y_set = false;
+
+  vector<string> svector = parseString(str, ',');
+  for(unsigned int i=0; i<svector.size(); i++) {
+    string param = tolower(biteStringX(svector[i], '='));
+    string value = svector[i];
+    if(param == "id")
+      id = value;
+    else if(param == "x") { x = atof(value.c_str()); x_set = true; }
+    else if(param == "y") { y = atof(value.c_str()); y_set = true; }
+  }
+
+  // Malformed alert
+  if((id == "") || !x_set || !y_set)
+    return(false);
+
+  // [Lab09] Mporei na laboume alert akoma kai gia hdh-swsmeno swimmer -> agnooume
+  if(m_rescued_ids.count(id))
+    return(true);
+
+  // [Lab09] Hdh gnwstos energos swimmer -> dedupe, kamia allagh sto path
+  if(m_swimmers.count(id))
+    return(true);
+
+  // Kainourios swimmer -> apo8hkeush + zhthse replan
+  XYPoint pt(x, y);
+  pt.set_label(id);
+  m_swimmers[id] = pt;
+  m_total_new_swimmers++;
+  m_plan_pending = true;
+  reportEvent("New swimmer id=" + id + " (" + doubleToString(x,1) +
+              "," + doubleToString(y,1) + ")");
   return(true);
 }
 
 //---------------------------------------------------------
 // Procedure: handleMailFoundSwimmer()
+//   String: "id=01, finder=abe"
+//   [Lab09] enas swimmer swstike (apo emas h apo allon). Ton bgazoume apo
+//           ta energa kai ksana-sxediazoume xwris auton.
 
 bool GenRescue::handleMailFoundSwimmer(string str)
 {
+  m_total_found_msgs++;
+
+  string id, finder;
+  vector<string> svector = parseString(str, ',');
+  for(unsigned int i=0; i<svector.size(); i++) {
+    string param = tolower(biteStringX(svector[i], '='));
+    string value = svector[i];
+    if(param == "id")
+      id = value;
+    else if(param == "finder")
+      finder = value;
+  }
+
+  if(id == "")
+    return(false);
+
+  m_rescued_ids.insert(id);
+
+  // An htan stous energous, bgale ton kai zhthse replan
+  if(m_swimmers.count(id)) {
+    m_swimmers.erase(id);
+    m_plan_pending = true;
+    reportEvent("Swimmer id=" + id + " rescued by " + finder + " -> replan");
+  }
   return(true);
 }
 
 //---------------------------------------------------------
 // Procedure: postShortestPath()
+//   [Lab09] Xtizei greedy nearest-neighbor path panw stous ENERGOUS
+//           swimmers, ksekinwntas apo th 8esh tou oxhmatos (NAV).
 
 void GenRescue::postShortestPath()
 {
-  // If path has not been set, determine a random path of 9
-  // points, and make a greedy path from ownship start position.
-  // Once it has been set, don't change it. But keep posting it
-  // once every 20 iterations.
-  
-  if(m_path.size() == 0) {
-    XYFieldGenerator generator;
-    generator.addPolygon("-184,-5:-188, -14:-130,-44:-106,-3");
-    generator.addPolygon("-85,-3:-89,-8:-51,-1");
-    generator.addPolygon("-78,-74:-54,-32:-104,-53");
-    generator.setBufferDist(7);
-    generator.setMaxTries(1000);
-    generator.generatePoints(9);
-    
-    vector<XYPoint> pts = generator.getPoints();
-    
-    for(unsigned int i=0; i<pts.size(); i++) {
-      XYPoint pt = pts[i];
-      m_path.add_vertex(pt.x(), pt.y());
-    }
-    // Seglist needs a name, refer when drawging and erasing
-    m_path.set_label("one");    
-    XYSegList segl;
-    segl.add_vertex(m_nav_x, m_nav_y);
-
-    m_path = greedyPath(m_path, m_nav_x, m_nav_y);
-    
-    // Seglist needs a name, refer when drawging and erasing
-    segl.set_label("one");
+  // Den exoun apomeinei swimmers -> tipota na sxediasoume
+  if(m_swimmers.size() == 0) {
+    postNullPath();
+    return;
   }
-  
+
+  // Maze ola ta shmeia twn energwn swimmers
+  XYSegList swim_pts;
+  map<string, XYPoint>::iterator q;
+  for(q=m_swimmers.begin(); q!=m_swimmers.end(); q++)
+    swim_pts.add_vertex(q->second.x(), q->second.y());
+
+  // Greedy diataksh apo th 8esh tou oxhmatos
+  m_path = greedyPath(swim_pts, m_nav_x, m_nav_y);
+  m_path.set_label("rescue_" + m_vname);
+
   Notify("VIEW_SEGLIST", m_path.get_spec());
 
-  string update_var = "SURVEY_UPDATE";
   string update_str = "points = " + m_path.get_spec_pts();
-
-  Notify(update_var, update_str);
-  reportEvent("SURVEY_UPDATE=" + update_str);
+  Notify(m_update_var, update_str);
+  reportEvent(m_update_var + "=" + update_str);
+  m_plans_posted++;
 }
 
 //---------------------------------------------------------
 // Procedure: postNullPath()
-//   Purpose: If a found swimmer represents the last swimmer
-//            to be found, then post a survey update essentially
-// 
+//   [Lab09] Den emeinan swimmers: dwse ws monadiko waypoint th
+//           trexousa 8esh, wste to waypt_survey na "oloklhrwsei"
+//           (kai na pyrodothsei to endflag RETURN=true).
 
 void GenRescue::postNullPath()
 {
-#if 0
   if(!m_nav_x_set || !m_nav_y_set)
     return;
-  if(m_map_pts.size() != 0)
-    return;
-  
+
   XYSegList segl;
   segl.add_vertex(m_nav_x, m_nav_y);
-  
-  // Seglist needs a name, refer when drawging and erasing
-  segl.set_label("one");
+  segl.set_label("rescue_" + m_vname);
+
   Notify("VIEW_SEGLIST", segl.get_spec());
 
-  string update_var = "SURVEY_UPDATE";
   string update_str = "points = " + segl.get_spec_pts();
-
-  Notify(update_var, update_str);
-  reportEvent("SURVEY_UPDATE=" + update_str);
-#endif
+  Notify(m_update_var, update_str);
+  reportEvent(m_update_var + "=" + update_str + " (no swimmers left)");
 }
 
 
@@ -221,5 +286,23 @@ void GenRescue::postNullPath()
 
 bool GenRescue::buildReport()
 {
+  m_msgs << "Vehicle Name: " << m_vname                          << endl;
+  m_msgs << "Update Var:   " << m_update_var                     << endl;
+  m_msgs << "NAV Fix:      ";
+  if(m_nav_x_set && m_nav_y_set)
+    m_msgs << "(" << doubleToString(m_nav_x,1) << ", "
+           << doubleToString(m_nav_y,1) << ")"                   << endl;
+  else
+    m_msgs << "<none yet>"                                       << endl;
+  m_msgs << "---------------------------------------"            << endl;
+  m_msgs << "SWIMMER_ALERTs received: " << m_total_alerts        << endl;
+  m_msgs << "Unique swimmers known:   " << m_total_new_swimmers  << endl;
+  m_msgs << "FOUND_SWIMMER msgs:      " << m_total_found_msgs    << endl;
+  m_msgs << "Swimmers rescued:        " << m_rescued_ids.size()  << endl;
+  m_msgs << "Swimmers still active:   " << m_swimmers.size()     << endl;
+  m_msgs << "Paths posted:            " << m_plans_posted        << endl;
+  m_msgs << "Plan pending:            " << boolToString(m_plan_pending) << endl;
+  m_msgs << "Current path length:     " << m_path.size() << " pts" << endl;
+
   return(true);
 }
--- src/pGenRescue/GenRescue.h
+++ src/pGenRescue/GenRescue.h
@@ -3,6 +3,8 @@
 /*    ORGN: MIT                                             */
 /*    FILE: GenRescue.h                                     */
 /*    DATE: April 18th, 2022                                */
+/*    MOD : Lab 09 Assignment 4 - dynamic rescue planning   */
+/*          (Evangelos Siatis)                              */
 /************************************************************/
 
 #ifndef P_GEN_RESCUE_HEADER
@@ -11,8 +13,10 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <set>
 #include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
 #include "XYPoint.h"
+#include "XYSegList.h"
 #include "XYPolygon.h"
 
 class GenRescue : public AppCastingMOOSApp
@@ -28,7 +32,7 @@ class GenRescue : public AppCastingMOOSApp
   bool OnStartUp();
   bool buildReport();
   void RegisterVariables();
-  
+
  protected:
   bool handleMailNewSwimmer(std::string);
   bool handleMailFoundSwimmer(std::string);
@@ -38,13 +42,26 @@ class GenRescue : public AppCastingMOOSApp
 
  private: // Config variables
   std::string m_vname;
-  
+  std::string m_update_var;   // [Lab09] var pou akouei to waypt_survey (SURVEY_UPDATE)
+
  private: // State variables
   XYSegList  m_path;
   double     m_nav_x;
   double     m_nav_y;
   bool       m_nav_x_set;
-  bool       m_nav_y_set;    
+  bool       m_nav_y_set;
+
+  // [Lab09] energoi swimmers pou DEN exoun swthei akoma: id -> shmeio
+  std::map<std::string, XYPoint> m_swimmers;
+  // [Lab09] ids pou exoun hdh swthei (apo emas h apo allon) -> ta agnooume
+  std::set<std::string>          m_rescued_ids;
+
+  bool   m_plan_pending;        // [Lab09] xreiazetai neo path (nees plhrofories)
+  // metrites gia to AppCast report
+  unsigned int m_total_alerts;        // posa SWIMMER_ALERT eidame synolika
+  unsigned int m_total_new_swimmers;  // posoi MONADIKOI swimmers
+  unsigned int m_total_found_msgs;    // posa FOUND_SWIMMER eidame
+  unsigned int m_plans_posted;        // posa paths postaristikan
 };
 
-#endif 
+#endif
